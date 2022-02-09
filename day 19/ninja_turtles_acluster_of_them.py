8ca14a76e21bfce00763ff7ffa62a997493aa14c
neilcayton <87573719+neilcayton@users.noreply.github.com>
2022-02-10 02:29:50 +0800

    Add files via upload

--- day 19/ninja_turtles_acluster_of_them.py
+++ day 19/ninja_turtles_acluster_of_them.py
@@ -1,19 +1,37 @@
 from turtle import Turtle, Screen
-from random import random
+import random
 
-
+is_race_on = False
 t = Turtle()
-r = random()
-screen = Screen()
-screen.setup(500, 400)
-screen.textinput(title="Make your Bet", prompt="Which turtle will win the race? Enter Color: ")
-colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
-for i in range(len(colors)):
-    colors[i] = Turtle()
-    t.shape("turtle")
-    my_color = colors[i]
-    t.color(f"{my_color}")
-    t.goto(x=-220, y=-100 + 10 * i)
-    screen.exitonclick()
-#
+screen = Screen()
+screen.setup(700, 570)
+screen.bgpic("Capture.PNG")
+user_bet = screen.textinput(title="Make your Bet", prompt="Which turtle will win the race? Enter Color: ")
+
+colors = ["red", "cyan", "pink", "yellow", "green", "blue", "purple"]
+all_turtles = []
+for i in range(0, 7):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[i])
+    new_turtle.penup()
+    new_turtle.goto(x=-220, y=-120 + 40 * i)
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+
+while is_race_on:
+    for turtle in all_turtles:
+        random_distance = random.randint(0, 10)
+        turtle.fd(random_distance)
+        if turtle.xcor() > 185:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if user_bet == winning_color:
+                print("You Won")
+            else:
+                print("You Lost")
+
+screen.exitonclick()
