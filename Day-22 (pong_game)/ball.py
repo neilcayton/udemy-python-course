31e2b83f05952ab127a8fd0e12967468c482269c
neilcayton <87573719+neilcayton@users.noreply.github.com>
2022-08-23 08:37:45 -0700

    Add files via upload

--- Day-22 (pong_game)/ball.py
+++ Day-22 (pong_game)/ball.py
@@ -5,7 +5,7 @@ class Ball(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.speed(20)
+        self.speed(50)
         self.color("white")
         self.shape("circle")
         self.shapesize(1)
@@ -13,6 +13,7 @@ class Ball(Turtle):
         self.showturtle()
         self.ycor_move = 10
         self.xcor_move = 10
+        self.move_speed = 0.1
 
     def move(self):
         new_xcor = self.xcor() + self.xcor_move
@@ -24,6 +25,10 @@ class Ball(Turtle):
 
     def bounce_x(self):
         self.xcor_move *= -1
+        self.move_speed *= 0.9
 
     def reset_position(self):
         self.goto(0, 0)
+
+    def reset_speed(self):
+        self.move_speed = 0.1
--- Day-22 (pong_game)/main.py
+++ Day-22 (pong_game)/main.py
@@ -1,8 +1,9 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
-
+from scoreboard import Scoreboard
 import time
+
 
 screen = Screen()
 
@@ -11,7 +12,7 @@ screen.title("Ping-pong Game")
 screen.bgcolor("black")
 
 screen.tracer(0)
-
+scoreboard = Scoreboard()
 paddle_l = Paddle(pos=350)
 paddle_r = Paddle(pos=-350)
 ball = Ball()
@@ -23,20 +24,26 @@ screen.onkeypress(paddle_r.go_down, "s")
 screen.onkeypress(paddle_l.go_up, "Up")
 screen.onkeypress(paddle_l.go_down, "Down")
 
-
 game_is_on = True
 while game_is_on:
-    time.sleep(.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
-    if ball.ycor() > 390 or ball.ycor() < -390:
+    # print(ball.xcor())
+    if ball.ycor() > 380 or ball.ycor() < -380:
         ball.bounce_y()
     # game_is_on = False
-    if paddle_l.distance(ball) < 50 and ball.xcor() > 340\
-            or paddle_r.distance(ball) < 50 and ball.xcor() < -340:
+    if paddle_l.distance(ball) < 50 and ball.xcor() > 330 \
+            or paddle_r.distance(ball) < 50 and ball.xcor() < -330:
+
+        print("made contact")
         ball.bounce_x()
-    if ball.xcor() > 380 or ball.xcor() < -380:
+    if ball.xcor() > 390:
         ball.reset_position()
-
-
+        scoreboard.l_point()
+        ball.reset_speed()
+    if ball.xcor() < -390:
+        ball.reset_position()
+        scoreboard.r_point()
+        ball.reset_speed()
 screen.exitonclick()
